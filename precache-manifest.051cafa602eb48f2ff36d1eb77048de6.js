68394426015614d75a84628a04826e12ebac8263
robinnong <robin.nong@hotmail.com>
2020-04-28 23:52:03 -0400

    Updates

---
+++ precache-manifest.051cafa602eb48f2ff36d1eb77048de6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5cd937fe971fcb48b96f81dcfdeb8a",
+    "url": "/poketypes/index.html"
+  },
+  {
+    "revision": "0eae77cb872cddc9b9d2",
+    "url": "/poketypes/static/css/main.a2f4b356.chunk.css"
+  },
+  {
+    "revision": "1bd6cc1fa9770eb38b57",
+    "url": "/poketypes/static/js/2.03e1680f.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/poketypes/static/js/2.03e1680f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eae77cb872cddc9b9d2",
+    "url": "/poketypes/static/js/main.66a0da53.chunk.js"
+  },
+  {
+    "revision": "d1d3279f272ffe825640",
+    "url": "/poketypes/static/js/runtime-main.5f56d3e3.js"
+  },
+  {
+    "revision": "b928cfc6717606d2d88f47e3648347e7",
+    "url": "/poketypes/static/media/treeBackground1.b928cfc6.png"
+  },
+  {
+    "revision": "8584fd235fa57779ce127e610d4455c6",
+    "url": "/poketypes/static/media/treeBackground2.8584fd23.png"
+  }
+]);
