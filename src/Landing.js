dcc560b14841de02e0743a2e536792a205bbf0a4
robinnong <robin.nong@hotmail.com>
2020-04-29 22:22:31 -0400

    Updated styling

--- src/Landing.js
+++ src/Landing.js
@@ -4,8 +4,10 @@ function Landing(props) {
     return (
         <> 
             <h1 className="animated bounceIn">PokeTypes</h1>
-            <p>Test your typing skills and catch as many Pokemon as you can!</p>
-            <p>Type the names of Pokemon as they appear on the screen.</p>
+            <div className="landingText">
+                <p>Test your typing skills and catch as many Pokemon as you can!</p>
+                <p>Type the names of Pokemon as they appear on the screen.</p>
+            </div>
             <button className="startButton" type="button" onClick={props.startGame}>Start Game</button>
             <button className="leaderboardButton" type="button" onClick={props.showLeaderboard}>Leaderboard</button>
         </>
