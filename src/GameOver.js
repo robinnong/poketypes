d291644a86a9f2fa803c13f5b9bb03326b60bc8d
robinnong <robin.nong@hotmail.com>
2020-05-02 18:38:41 -0400

    Fixed wrapper padding and image height on mobile

--- src/GameOver.js
+++ src/GameOver.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const GameOver = (props) =>{ 
     return (
-        <div className="gameOver">
+        <div className="gameOver wrapper">
             <h2 className="animated bounceIn">Game Over</h2>
-            <p className="endMessage animated fadeIn">You caught <span>{props.finalScore}</span> Pokemon!</p>  
+            <p className="endMessage animated fadeInUp">You caught <span>{props.finalScore}</span> Pokemon!</p>  
             <div className="buttonContainer">
                 <button type="button" className="redButton" onClick={props.replay}>
                     Replay<i className="fas fa-reply" aria-hidden="true"></i>
